a845f47126f3f12d6b0b8d5f864baa8c975be752
luoqiliu <luoqi33164@gmail.com>
2016-12-14 17:12:16 +0800

    plugin

---
+++ theme/index.js
@@ -0,0 +1,12 @@
+(function addNavDiv() {
+    var book = document.getElementsByClassName("book")[0];
+    var parent = document.getElementsByTagName("body")[0];
+    var div = document.createElement("div");
+    var p = document.createElement("a");
+    p.href = "#";
+    p.setAttribute("id", "logo");
+    p.innerHTML = "holyIt";
+    div.setAttribute("id", "nav");
+    div.appendChild(p);
+    parent.insertBefore(div, book);
+})();
