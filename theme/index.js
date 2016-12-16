db31914e2be7b654bbd2234148358c21435273db
luoqiliu <luoqi33164@gmail.com>
2016-12-16 14:07:57 +0800

    s

--- theme/index.js
+++ theme/index.js
@@ -2,11 +2,11 @@
     var book = document.getElementsByClassName("book")[0];
     var parent = document.getElementsByTagName("body")[0];
     var div = document.createElement("div");
-    var p = document.createElement("a");
-    p.href = "#";
-    p.setAttribute("id", "logo");
-    p.innerHTML = "holyIt";
-    div.setAttribute("id", "nav");
-    div.appendChild(p);
-    parent.insertBefore(div, book);
+    //var p = document.createElement("a");
+    //p.href = "#";
+    //p.setAttribute("id", "logo");
+    //p.innerHTML = "holyIt";
+    //div.setAttribute("id", "nav");
+    //div.appendChild(p);
+    //parent.insertBefore(div, book);
 })();
